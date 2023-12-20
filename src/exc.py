3f9e999cb74ee5fb3b765726eff9fdbfac70ab74
Marco Palmisano <78793610+Marcutyo@users.noreply.github.com>
2023-12-20 22:10:39 +0100

    🛠️ refactor: delete InvalidInstantNameError

--- src/exc.py
+++ src/exc.py
@@ -6,9 +6,3 @@ class InstantNotFoundError(InstantError):
     """Raised when an instant is not found."""
     def __init__(self, name: str):
         super().__init__(f"Instant with name {name!r} not found.")
-
-
-class InvalidInstantNameError(InstantError):
-    """Raised when an invalid instant name is provided."""
-    def __init__(self):
-        super().__init__("Instant name must not empty and not None.")
